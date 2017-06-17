082f6925b747f5a97f9a4f8845740ea277a1e5f5
LDQ-first <178848890@qq.com>
2017-06-17 14:14:24 +0800

    modify

--- src/CommentList.js
+++ src/CommentList.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 import Comment from './Comment'
 
 class CommentList extends Component {
-     static defalutProps = {
+     static defaultProps = {
          comments: []   
     }
 
     render() {
         return (
             <div>
-                {comments.map( (comment, i) => 
+                {this.props.comments.map( (comment, i) => 
                     <Comment key={i} comment={comment}></Comment>)
                 }
             </div>
